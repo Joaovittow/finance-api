cb82b57b2ef5f3d4cf8c0f50c8ae585a2924ffa1
joaovittow <joaovitorseixasn13@outlook.com>
2025-10-13 21:21:24 -0300

    refactor(prisma/schema): Change `dataPrimeiraParcela` and `dataVencimento` types from `DateTime` to `String` for better compatibility

---
+++ prisma/migrations/20251014002033_change_dates_to_string/migration.sql
@@ -0,0 +1,5 @@
+-- AlterTable
+ALTER TABLE "despesas" ALTER COLUMN "dataPrimeiraParcela" SET DATA TYPE TEXT;
+
+-- AlterTable
+ALTER TABLE "parcelas" ALTER COLUMN "dataVencimento" SET DATA TYPE TEXT;
